8f63b31c7b94483701932d82ea074442c5f0077a
yangchenD2xi <yangchend2xi@163.com>
2025-11-08 08:39:40 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -7,7 +7,7 @@ sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/coll
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 
 #添加编译日期标识
-sed -i "s#ImmortalWRT.*#ImmortalWRT 24.10.4 / Build：yangchenD2xi / Build time：$(date +%Y.%m.%d)
+sed -i "s#ImmortalWRT.*#ImmortalWRT 24.10.4 / Build：yangchenD2xi / Build time：$(date +%Y.%m.%d)/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
 WIFI_SH=$(find ./target/linux/{mediatek/filogic,qualcommax}/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
 WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
